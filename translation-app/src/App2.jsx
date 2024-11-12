b60899e4df914dc6df528a7f497935552264288d
rajashekarcs2023 <vrajashekar2022mscs@gmail.com>
2024-11-11 23:35:43 -0800

    completed scalable code

--- translation-app/src/App2.jsx
+++ translation-app/src/App2.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
 import TranslationPage from './components/TranslationPage';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <TranslationPage />
-    </div>
+    <TranslationPage />
   );
 }
 
